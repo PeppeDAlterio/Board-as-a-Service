84e537f5cd4de35410b5d0f91d9daf721370c6bd
Fernando <dicostanzofernando1996@gmail.com>
2020-05-22 12:51:54 +0200

    asdsad

--- src/main/java/it/unina/sistemiembedded/utility/RedirectStream.java
+++ src/main/java/it/unina/sistemiembedded/utility/RedirectStream.java
@@ -5,6 +5,6 @@ public class RedirectStream {
     public static final String TEXT_AREA_COMUNICATION_SERVER= "TEXT_AREA_COMUNICATION_SERVER";
     public static final String TEXT_AREA_ACTION_SERVER = "TEXT_AREA_ACTION_SERVER";
     public static final String TEXT_AREA_SENDMESSAGE_CLIENT ="TEXT_AREA_SENDMESSAGE_CLIENT";
-    public static final String TEXT_AREA_DEGUB_CLEINT ="TEXT_AREA_DEGUB_CLEINT";
+    public static final String TEXT_AREA_DEGUB_CLIENT ="TEXT_AREA_DEGUB_CLEINT";
     public static final String TEXT_AREA_FLASH_CLIENT ="TEXT_AREA_FLASH_CLIENT";
 }
